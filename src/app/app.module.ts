028e71833a2f9f043c169909b9585a4f8de31cef
Subho45 <59506122+Subho45@users.noreply.github.com>
2021-03-05 10:23:22 +0530

    commit second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NewHomepageComponent } from './new-homepage/new-homepage.component';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewHomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatInputModule, BrowserAnimationsModule,MatButtonModule,MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-homepage/new-homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-homepage',
+  templateUrl: './new-homepage.component.html',
+  styleUrls: ['./new-homepage.component.scss']
+})
+export class NewHomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
